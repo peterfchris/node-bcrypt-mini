b47715980ce34fcc41d4249cd77aa0634452e3ea
Peter Christensen <peter.f.chris@gmail.com>
2019-05-23 12:23:35 -0600

    finished mini

--- server/index.js
+++ server/index.js
@@ -1,27 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const massive = require('massive');
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const bcrypt = require('bcryptjs')
+const massive = require('massive')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-let { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+let { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 
 app.use(
-  session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  })
-);
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60
+        }
+    })
+)
 
-massive(CONNECTION_STRING).then(db => {
-  app.set('db', db);
-});
+massive(CONNECTION_STRING).then((db) => {
+    app.set('db', db)
+})
+
+app.post('/auth/signup', async (req, res) => {
+    const { email, password } = req.body
+    const db = req.app.get('db')
+    const foundUser = await db.check_user_exists(email)
+    if (foundUser[0]) return res.status(409).send('Email taken')
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const newUser = await db.create_user([email, hash])
+    req.session.user = { id: newUser[0].id, email: newUser[0].email }
+    res.status(200).send(req.session.user)
+})
+
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body
+    const db = req.app.get('db')
+    const foundUser = await db.check_user_exists(email)
+    if (!foundUser[0]) return res.status(409).send('Incorrect Email')
+    const authenticated = bcrypt.compareSync(password, foundUser[0].user_password)
+    if (authenticated) {
+        req.session.user = { id: foundUser[0].id, email: foundUser[0].email }
+        res.status(200).send(req.session.user)
+    } else {
+        res.status(401).send('Incorrect Email or password')
+    }
+})
+
+app.get('/auth/logout', (req,res) => {
+  req.session.destroy()
+  res.sendStatus(200)
+})
+
+app.get('/auth/user', (req, res) => {
+  if(req.session.user){
+    res.status(200).send(req.session.user)
+  } else {
+    res.status(401).send('Please Log In')
+  }
+})
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Listening on port: ${SERVER_PORT}`);
-});
+    console.log(`Listening on port: ${SERVER_PORT}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,62 +1,79 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      loggedInUser: {}
-    };
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            loggedInUser: {}
+        }
+    }
 
-  async login() {}
+    async login() {
+      const {email, password} = this.state
+      const result = await axios.post('/auth/login',  {
+        email,
+        password
+      })
+      this.setState({loggedInUser: result.data, email: '', password: ''})
+    }
 
-  async signup() {}
+    async signup() {
+        const { email, password } = this.state
+        const result = await axios.post('/auth/signup', {
+            email,
+            password
+        })
+        this.setState({ loggedInUser: result.data, email: '', password: '' })
+    }
 
-  logout() {}
+    logout() {
+      axios.get('/auth/logout')
+      this.setState({loggedInUser: {}})
+    }
 
-  render() {
-    let { loggedInUser, email, password } = this.state;
-    return (
-      <div className="form-container done">
-        <div className="login-form">
-          <h3>Auth w/ Bcrypt</h3>
-          <div>
-            <input
-              value={email}
-              onChange={e => this.setState({ email: e.target.value })}
-              type="text"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <input
-              value={password}
-              type="password"
-              onChange={e => this.setState({ password: e.target.value })}
-              placeholder="password"
-            />
-          </div>
-          {loggedInUser.email ? (
-            <button onClick={() => this.logout()}>Logout</button>
-          ) : (
-            <button onClick={() => this.login()}>Login</button>
-          )}
-          <button onClick={() => this.signup()}>Sign up</button>
-        </div>
+    render() {
+        let { loggedInUser, email, password } = this.state
+        return (
+            <div className='form-container done'>
+                <div className='login-form'>
+                    <h3>Auth w/ Bcrypt</h3>
+                    <div>
+                        <input
+                            value={email}
+                            onChange={(e) => this.setState({ email: e.target.value })}
+                            type='text'
+                            placeholder='Email'
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value={password}
+                            type='password'
+                            onChange={(e) => this.setState({ password: e.target.value })}
+                            placeholder='password'
+                        />
+                    </div>
+                    {loggedInUser.email ? (
+                        <button onClick={() => this.logout()}>Logout</button>
+                    ) : (
+                        <button onClick={() => this.login()}>Login</button>
+                    )}
+                    <button onClick={() => this.signup()}>Sign up</button>
+                </div>
 
-        <hr />
+                <hr />
 
-        <h4>Status: {loggedInUser.email ? 'Logged In' : 'Logged Out'}</h4>
-        <h4>User Data:</h4>
-        <p> {loggedInUser.email ? JSON.stringify(loggedInUser) : 'No User'} </p>
-        <br />
-      </div>
-    );
-  }
+                <h4>Status: {loggedInUser.email ? 'Logged In' : 'Logged Out'}</h4>
+                <h4>User Data:</h4>
+                <p> {loggedInUser.email ? JSON.stringify(loggedInUser) : 'No User'} </p>
+                <br />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
